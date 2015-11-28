4c58eb984cde793e4d5f439e195fac94a33a20b4
Tatiana Soukiassian <tatiana.soukiassian@digital.cabinet-office.gov.uk>
2015-11-28 14:49:06 +0000

    clean up and finish deleting to do list items feature

--- spec/features/todo_items/delete_spec.rb
+++ spec/features/todo_items/delete_spec.rb
@@ -16,7 +16,7 @@ describe "Deleting todo items" do
     within "#todo_item_#{todo_item.id}" do
       click_link "Delete"
     end
-    expect(page).to have_content("Todo list item was deleted.")
+    expect(page).to have_content("Todo list item was deleted")
   end
 
 end
