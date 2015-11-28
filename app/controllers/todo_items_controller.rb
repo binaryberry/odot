7fec546697046cfeb338173552d3194c73d6d801
Tatiana Soukiassian <tatiana.soukiassian@digital.cabinet-office.gov.uk>
2015-11-28 17:53:22 +0000

    Add marked as complete

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -2,7 +2,6 @@ class TodoItemsController < ApplicationController
 
   before_action :find_todo_list
 
-
   def index
   end
 
@@ -19,6 +18,12 @@ class TodoItemsController < ApplicationController
   		flash[:error] = "There was a problem adding that todo list item"
   		render action: :new
   	end
+  end
+
+  def complete
+    @todo_item = @todo_list.todo_items.find(params[:id])
+    @todo_item.update_attribute(:completed_at, Time.now)
+    redirect_to todo_list_todo_items_path, notice: "Todo item marked as complete."
   end
 
   def edit
@@ -55,7 +60,6 @@ class TodoItemsController < ApplicationController
   def find_todo_list
     @todo_list = TodoList.find(params[:todo_list_id])
   end
-
 
   def todo_item_params
   	params[:todo_item].permit(:content)
